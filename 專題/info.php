a2dde0b0428f84f046137b45ad1fc9dae847bf88
Geralidne <118123089+Geralidne@users.noreply.github.com>
2023-04-13 23:59:25 +0800

    Add files via upload

--- 專題/info.php
+++ 專題/info.php
@@ -1,13 +1,81 @@
 <!DOCTYPE html>
-<html lang=' zh-TW'>
-<head>
-<meta charset="utf-8"/>
-<title>化妝品碳盤查</title>
-<meta http-equiv="refresh" content="60" />
-</head>
 <html>
-<body>
-
-
-</body>
-</html>
+  <head>
+    <meta charset="UTF-8">
+    <title>化妝品類別選擇</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f5f5;
+      }
+      h1 {
+        text-align: center;
+        color: #333;
+      }
+      .button-group {
+        max-width: 600px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+      .button {
+        padding: 10px;
+        margin: 10px;
+        background-color: #fff;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        cursor: pointer;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        transition: all 0.3s ease;
+        width: 200px;
+        text-align: center;
+      }
+      .button:hover {
+        transform: translateY(-5px);
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+      }
+      .button img {
+        display: block;
+        margin: 0 auto;
+        max-width: 100%;
+        height: auto;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>化妝品類別選擇按鈕</h1>
+    <div class="button-group">
+      <a href="https://www.example.com/foundation">
+        <div class="button">
+          <img src="https://via.placeholder.com/200x200.png?text=Foundation" alt="Foundation">
+          <span>粉底</span>
+        </div>
+      </a>
+      <a href="https://www.example.com/lipstick">
+        <div class="button">
+          <img src="https://via.placeholder.com/200x200.png?text=Lipstick" alt="Lipstick">
+          <span>口紅</span>
+        </div>
+      </a>
+      <a href="https://www.example.com/mascara">
+        <div class="button">
+          <img src="https://via.placeholder.com/200x200.png?text=Mascara" alt="Mascara">
+          <span>睫毛膏</span>
+        </div>
+      </a>
+      <a href="https://www.example.com/eyeliner">
+        <div class="button">
+          <img src="https://via.placeholder.com/200x200.png?text=Eyeliner" alt="Eyeliner">
+          <span>眼線液</span>
+        </div>
+      </a>
+      <a href="https://www.example.com/blush">
+        <div class="button">
+          <img src="https://via.placeholder.com/200x200.png?text=Blush" alt="Blush">
+          <span>腮紅</span>
+        </div>
+      </a>
+      <a href="https://www.example.com/eyeshadow">
+        <div class="button">
+          <img src="https://via.placeholder
